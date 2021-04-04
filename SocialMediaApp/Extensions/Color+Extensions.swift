27c8ab33f0fb6fff355aea86b5216a5fd421d829
Albert <alberto.gaudicos@appetiser.com.au>
2021-04-04 23:45:08 +0800

    Add new color extension

--- SocialMediaApp/Extensions/Color+Extensions.swift
+++ SocialMediaApp/Extensions/Color+Extensions.swift
@@ -12,4 +12,5 @@ extension Color {
     static let appBlueColor = Color(red: 6/255, green: 88/255, blue: 253/255, opacity: 1)
     static let fillButtonColor = Color(red: 112/255, green: 163/255, blue: 245/255, opacity: 1)
     static let fillButtonBorderColor = Color(red: 38/255, green: 128/255, blue: 235/255, opacity: 1)
+    static let appLightBlueColor = Color(red: 6/255, green: 88/255, blue: 253/255, opacity: 0.05)
 }
