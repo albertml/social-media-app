0510d3bf78e40ae8a4ebe3d36ade82d312b3ec00
Albert <alberto.gaudicos@appetiser.com.au>
2021-04-04 23:45:42 +0800

    Add Upload photo UI with input of title and upload of image

--- SocialMediaApp/Views/Post/PostView.swift
+++ SocialMediaApp/Views/Post/PostView.swift
@@ -8,8 +8,103 @@
 import SwiftUI
 
 struct PostView: View {
+    
+    init() {
+        UINavigationBar.appearance().barStyle = .default
+        UINavigationBar.appearance().backgroundColor = .clear
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
+        UINavigationBar.appearance().shadowImage = UIImage()
+    }
+    
+    var navigationTitle: Text {
+        Text("Create a Post")
+    }
+    
+    var leadingButton: some View {
+        Button {
+            
+        } label: {
+            SFSymbol.arrowLeft
+        }
+    }
+    
+    var trailingButton: some View {
+        Button {
+            
+        } label: {
+            Text("Save Draft")
+                .font(.system(size: 14, weight: .regular))
+                .foregroundColor(.gray)
+        }
+    }
+    
+    var inputTitleView: some View {
+        VStack(alignment: .leading) {
+            Text("Add a title")
+                .font(.system(size: 14, weight: .regular))
+                .foregroundColor(.gray)
+            TextField("", text: $title)
+                .font(.system(size: 16, weight: .bold))
+                .foregroundColor(.black)
+            Divider()
+        }
+    }
+    
+    var uploadPhotoView: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(
+                    Color.appBlueColor,
+                    style: StrokeStyle(
+                        lineWidth: 1,
+                        lineCap: .round,
+                        lineJoin: .miter,
+                        miterLimit: 0,
+                        dash: [5, 5],
+                        dashPhase: 0)
+                )
+                .frame(height: 176)
+                .foregroundColor(Color.appLightBlueColor)
+            VStack(spacing: 10) {
+                Image("upload-cloud")
+                    .resizable()
+                    .frame(width: 46, height: 38)
+                Text("Upload a photo")
+                    .font(.system(size: 16, weight: .bold))
+                    .foregroundColor(.appBlueColor)
+                Text("Supported formats PNG, GIF or JPG.\n Photos are usually 400 x 300 or 800 x 600.")
+                    .multilineTextAlignment(.center)
+                    .font(.system(size: 14, weight: .regular))
+                    .foregroundColor(.appBlueColor)
+            }
+            .padding(.horizontal)
+        }
+        .onTapGesture {
+            debugPrint("Browse images")
+        }
+    }
+    
+    @State private var title: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(alignment: .leading,  spacing: 20) {
+                Spacer()
+                    .frame(height: 10)
+                inputTitleView
+                uploadPhotoView
+                Spacer()
+            }
+            .padding(.horizontal)
+            .navigationTitle(navigationTitle)
+            .navigationBarItems(
+                leading: leadingButton,
+                trailing: trailingButton
+            )
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .accentColor(.primary)
     }
 }
 
